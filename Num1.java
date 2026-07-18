e294166cedc8dcc39d7e372c70477f84741afac6
Lakshanika T G K <lakshanika2006@gmail.com>
2026-07-18 10:36:30 +0000

    Add Num1 java  program

---
+++ Num1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Num1{
+    public static void main(String[] args){
+        int n = 5;
+     
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                System.out.print("1 ");
+            }
+            System.out.println();
+        }
+    }
+}
