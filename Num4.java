9be088b0235b5651d399080094d935be2e9c932c
Lakshanika T G K <lakshanika2006@gmail.com>
2026-07-18 11:06:27 +0000

    Add Num4.java

---
+++ Num4.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Num4{
+    public static void main(String[] args){
+        int n=5;
+
+        for(int i=1,p=0;i<=n;i++,p+=2){
+            for(int j=1;j<=i;j++){
+                System.out.print(p+"  ");
+            }
+            System.out.println();
+        }
+    }
+}
